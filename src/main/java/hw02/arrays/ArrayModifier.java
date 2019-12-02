ab338ce0a95c75fef1758344198a91adfcad511b
Julia4065 <jullia4065@gmail.com>
2019-12-02 22:26:00 +0200

    refactor code according to comments of support guys

--- src/main/java/hw02/arrays/ArrayModifier.java
+++ src/main/java/hw02/arrays/ArrayModifier.java
@@ -4,75 +4,52 @@ import java.util.Arrays;
 import java.util.Collections;
 
 public class ArrayModifier {
-    //ArrayHandler
 
-    public void sortArrayDesc(Integer[] originArray) {
-        printArrayOfReferences("Array before sorting: ", originArray);
-        Arrays.sort(originArray, Collections.reverseOrder());
-        printArrayOfReferences("Array after descending sorting: ", originArray);
+    void sortArrayDesc(int[] originArray) {
+        printPrimitiveArray("Array before sorting: ", originArray);
+
+        Integer[] convertedArray = Arrays.stream(originArray).boxed().toArray(Integer[]::new);
+        Arrays.sort(convertedArray, Collections.reverseOrder());
+        originArray = Arrays.stream(convertedArray).mapToInt(Integer::intValue).toArray();
+
+        printPrimitiveArray("Array after descending sorting: ", originArray);
     }
 
-    public double averageListNumbersOf(int[] originalArray) {
+    double findAverageSumOfListNumbersOf(int[] originalArray) {
         printPrimitiveArray("Original array: ", originalArray);
         int sumOfListElements = Arrays.stream(originalArray).sum();
 
         return (originalArray.length == 0) ? 0 : (double) sumOfListElements / originalArray.length;
     }
 
-    private void printArrayOfReferences(String message, Object[] arrayToPrint) {
-        System.out.print(message);
-        Arrays.stream(arrayToPrint).forEach(n -> System.out.print(n + " "));
-        System.out.print("\n");
+    int getTheSumOfPositiveElements(int[] array) {
+        printPrimitiveArray("Original array: ", array);
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 0) {
+                sum += array[i];
+            }
+        }
+        return sum;
+    }
+
+    void replaceDuplicateElementsWithZero(int[] arrayWithDuplicates) {
+        printPrimitiveArray("Origin array: ", arrayWithDuplicates);
+
+        for (int i = 0; i < arrayWithDuplicates.length; i++) {
+            int elForComparison = arrayWithDuplicates[i];
+            for (int j = i + 1; j < arrayWithDuplicates.length; j++) {
+                if (arrayWithDuplicates[j] == elForComparison) {
+                    arrayWithDuplicates[j] = 0;
+                }
+            }
+        }
+        printPrimitiveArray("Array after all duplicates were replaced with zero: ", arrayWithDuplicates);
     }
 
     private void printPrimitiveArray(String message, int[] arrayToPrint) {
         System.out.print(message);
         Arrays.stream(arrayToPrint).forEach(n -> System.out.print(n + " "));
         System.out.print("\n");
-    }
-
-
-    public int sumOfPositiveElements(int[] array) {
-        printPrimitiveArray("Original array: ", array);
-        int sum = 0;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] < 0) {
-                continue;
-            }
-            sum += array[i];
-        }
-        return sum;
-    }
-
-    public void replaceDuplicateElementsWithZero(int[] array) {
-        printPrimitiveArray("Origin array: ", array);
-        int[] arrayWithZeroInsteadOfDuplicates = replaceDuplicatedWithZero(array, findDuplicates(array));
-        printPrimitiveArray("Array after all duplicates were replaced with zero: ", arrayWithZeroInsteadOfDuplicates);
-    }
-
-    private int[] replaceDuplicatedWithZero(int[] array, int[] duplicates) {
-        for (int i = 0; i < duplicates.length; i++) {
-            for (int j = 0; j < array.length; j++) {
-                if (duplicates[i] == array[j]) {
-                    array[j] = 0;
-                }
-            }
-        }
-        return array;
-    }
-
-    private int[] findDuplicates(int[] array) {
-        int[] duplicatesArray = new int[array.length];
-
-        for (int i = 0; i < array.length; i++) {
-            for (int j = i + 1; j < array.length; j++) {
-                if (array[i] == array[j]) {
-                    duplicatesArray[i] = array[j];
-                    break;
-                }
-            }
-        }
-
-        return duplicatesArray;
     }
 }
--- src/main/java/hw02/arrays/Main.java
+++ src/main/java/hw02/arrays/Main.java
@@ -1,7 +1,7 @@
 package hw02.arrays;
 
 public class Main {
-    private static Integer[] originArray = {2, 3, 1, 7, 11};
+    private static int[] originArray = {2, 3, 1, 7, 11};
     private static int[] originPrimitiveArray = {1, 2, 4, 4, 7};
     private static int[] originArrayWithPosAndNegElements = {1, -2, 4, -2, 8, -5};
     private static int[] originArrayWithDuplicates = {3, 2, 3, 1, 4, 2, 8, 3};
@@ -14,14 +14,12 @@ public class Main {
         arrayModifier.sortArrayDesc(originArray);
 
         System.out.println("Task 4");
-        System.out.println("Sum of all positive elements in the array: " + arrayModifier.sumOfPositiveElements(originArrayWithPosAndNegElements));
+        System.out.println("Sum of all positive elements in the array: " + arrayModifier.getTheSumOfPositiveElements(originArrayWithPosAndNegElements));
 
         System.out.println("Task 5");
-        System.out.println("Average sum of the list of numbers in array: " + arrayModifier.averageListNumbersOf(originPrimitiveArray));
+        System.out.println("Average sum of the list of numbers in array: " + arrayModifier.findAverageSumOfListNumbersOf(originPrimitiveArray));
 
         System.out.println("Task 6");
         arrayModifier.replaceDuplicateElementsWithZero(originArrayWithDuplicates);
-
-
     }
 }
