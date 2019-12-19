60bff564e83f427562892c7611d6c8abbfec229e
Julia4065 <jullia4065@gmail.com>
2019-12-20 00:00:18 +0200

    One more change

--- src/main/java/hw06/listManipulator/ListManipulator.java
+++ src/main/java/hw06/listManipulator/ListManipulator.java
@@ -4,17 +4,15 @@ import java.util.*;
 
 public class ListManipulator {
 
-    public static List<Integer> removeElementsThatDivisibleBy(List<Integer> list, int divisibleNumber) {
+    public static void removeElementsThatDivisibleBy(List<Integer> list, int divisibleNumber) {
         list.removeIf(n -> n % divisibleNumber == 0);
-        return list;
     }
 
-    public static List<String> replaceValueInList(List<String> list, String oldValue, String newValue) {
+    public static void replaceValueInList(List<String> list, String oldValue, String newValue) {
         Collections.replaceAll(list, oldValue, newValue);
         if (!list.contains(newValue)) {
             System.out.println("Value is not present in list so no replacement happens in the list");
         }
-        return list;
     }
 
     public static boolean isSharedElementExists(List<String> firstList, List<String> secondList) {
