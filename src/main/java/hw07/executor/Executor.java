002a776ecee1ab819beda78a3a8ea32cc261af0a
Julia4065 <jullia4065@gmail.com>
2019-12-23 13:12:21 +0200

    home work 7

---
+++ src/main/java/hw07/executor/Executor.java
@@ -0,0 +1,40 @@
+package hw07.executor;
+
+import hw07.library.LibraryReport;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+
+public class Executor {
+
+    public void demonstrateLibraryWorking() {
+        demonstrateLibraryWorkingWithSingleBook();
+        demonstrateLibraryWorkingWithMultipleBooks();
+    }
+
+    public void demonstrateLibraryWorkingWithSingleBook() {
+        LibraryReport libraryReport = new LibraryReport();
+        libraryReport.takeABook(LocalDate.of(2019, 10, 7), "Book1");
+        libraryReport.takeABook(LocalDate.of(2018, 11, 8), "Book2");
+        libraryReport.takeABook(LocalDate.of(2017, 12, 9), "Book3");
+
+        LocalDate requestedDate = LocalDate.of(2018, 11, 8);
+        String bookTitle = libraryReport.searchBookBy(requestedDate);
+        System.out.println("Book that was borrowed on " + requestedDate + " is " + bookTitle);
+
+        System.out.println("List of borrowed books: ");
+        libraryReport.findListOfBorrowedBooks().forEach(System.out::println);
+
+        System.out.println("Dates when books were borrowed: ");
+        libraryReport.findDatesWhenBooksWereBorrowed().forEach(System.out::println);
+    }
+
+    public void demonstrateLibraryWorkingWithMultipleBooks() {
+        LibraryReport libraryReport = new LibraryReport();
+        libraryReport.takeFewBooks(LocalDate.of(2019, 10, 31), Arrays.asList("Book7", "Book8", "Book9"));
+        libraryReport.takeFewBooks(LocalDate.of(2019, 10, 19), Arrays.asList("Book10", "Book11"));
+        libraryReport.takeFewBooks(LocalDate.of(2019, 10, 13), Arrays.asList("Book12"));
+
+        libraryReport.countBooksThatWereTakenForTheMonth(LibraryReport.MONTH.OCTOBER);
+    }
+}
---
+++ src/main/java/hw07/library/LibraryReport.java
@@ -0,0 +1,89 @@
+package hw07.library;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.util.*;
+
+import static java.util.Map.Entry.comparingByKey;
+import static java.util.stream.Collectors.toMap;
+
+public class LibraryReport {
+
+    private Map<LocalDate, String> libraryReport = new HashMap<>();
+    private Map<LocalDate, List<String>> libraryReportForMultipleBooksTaken = new HashMap<>();
+
+    public void takeABook(LocalDate date, String bookTitle) {
+        libraryReport.put(date, bookTitle);
+    }
+
+    public String searchBookBy(LocalDate bookBorrowDate) {
+        return libraryReport.getOrDefault(bookBorrowDate, "There are no books for this date");
+    }
+
+    public Collection<String> findListOfBorrowedBooks() {
+        return libraryReport.values();
+    }
+
+    public Set<LocalDate> findDatesWhenBooksWereBorrowed() {
+        return libraryReport.keySet();
+    }
+
+    public void takeFewBooks(LocalDate date, List<String> bookTitles) {
+        libraryReportForMultipleBooksTaken.put(date, bookTitles);
+    }
+
+    public void countBooksThatWereTakenForTheMonth(MONTH month) {
+        Map<LocalDate, Long> datesWithCountedBooksThatWereTaken = libraryReportForMultipleBooksTaken.entrySet().stream().collect(toMap(date -> date.getKey(), books -> books.getValue().stream().count()));
+        Map<LocalDate, Long> allDates = findAllDatesInMonth(2019, month.getNumberOfMonth()).stream().collect(toMap(key -> key, val -> 0L));
+
+        Map<LocalDate, Long> countedBooksForTheMonth = new HashMap<>(datesWithCountedBooksThatWereTaken);
+        allDates.forEach((k, v) -> countedBooksForTheMonth.merge(k, v, Long::max));
+
+        LinkedHashMap<LocalDate, Long> sortedCountedBooksForTheMonth = countedBooksForTheMonth.entrySet().stream()
+                .sorted(comparingByKey())
+                .collect(toMap(Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+
+        System.out.println("How many books were borrowed day by day for the month " + month);
+        System.out.println(sortedCountedBooksForTheMonth);
+    }
+
+    private List<LocalDate> findAllDatesInMonth(int year, int month) {
+        List<LocalDate> datesInMonth = new ArrayList<>();
+        SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar cal = Calendar.getInstance();
+        cal.set(year, month - 1, 0);
+
+        int daysInMonth = YearMonth.of(year, month).lengthOfMonth();
+        for (int i = 0; i < daysInMonth; i++) {
+            cal.add(Calendar.DAY_OF_MONTH, 1);
+            datesInMonth.add(LocalDate.parse(fmt.format(cal.getTime())));
+        }
+        return datesInMonth;
+    }
+
+    public enum MONTH {
+        JANUARY(1),
+        FEBRUARY(2),
+        MARCH(3),
+        APRIL(4),
+        MAY(5),
+        JUNE(6),
+        JULY(7),
+        AUGUST(8),
+        SEPTEMBER(9),
+        OCTOBER(10),
+        NOVEMBER(11),
+        DECEMBER(12);
+
+        private final int value;
+
+        MONTH(final int value) {
+            this.value = value;
+        }
+
+        public int getNumberOfMonth() {
+            return value;
+        }
+    }
+}
