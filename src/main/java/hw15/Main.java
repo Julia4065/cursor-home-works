6ddeeff6f8464afcc4e9f91c5d4640220e93dea3
Julia4065 <jullia4065@gmail.com>
2020-02-27 23:10:53 +0200

    home work 15 maven

---
+++ src/main/java/hw15/Main.java
@@ -0,0 +1,12 @@
+package hw15;
+
+import hw15.analyzer.song.Executor;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        new Executor().demonstrateHW();
+    }
+}
